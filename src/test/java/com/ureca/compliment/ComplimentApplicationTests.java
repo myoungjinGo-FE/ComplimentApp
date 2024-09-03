1f4a28f1c2c4acdc1d10d97f31c2e2d9f5c7154f
myoungjinGo-FE <cokemania2099@gmail.com>
2024-09-03 17:47:43 +0900

    initial commit

---
+++ src/test/java/com/ureca/compliment/ComplimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.compliment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
